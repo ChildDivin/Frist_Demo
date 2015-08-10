52e6e3a0ff9605399fb8629071a38d31edad1cc9
divine_child <tops@Tops31.local>
2015-08-10 13:00:47 +0530

    Initial Commit

---
+++ FirstApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Tops on 8/10/15.
+//  Copyright (c) 2015 Tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
